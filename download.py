facf34a5f15a8cb08a41fb19bbadd364bd258e23
Mani Kher <manixkher@gmail.com>
2025-02-09 00:26:45 +0000

    changed download

--- download.py
+++ download.py
@@ -1,14 +1,3 @@
 # download the full dataset
 from huggingface_hub import snapshot_download
-import zipfile
-import os
 snapshot_download(repo_id="osv5m/osv5m", local_dir="datasets/osv5m", repo_type='dataset')
-
-
-
-for root, dirs, files in os.walk("datasets/osv5m"):
-    for file in files:
-        if file.endswith(".zip"):
-            with zipfile.ZipFile(os.path.join(root, file), 'r') as zip_ref:
-                zip_ref.extractall(root)
-                os.remove(os.path.join(root, file))
